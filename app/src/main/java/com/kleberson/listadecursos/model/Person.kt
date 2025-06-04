5c4e8d0eb59865f6164be392e219be64bb436c65
kleberson154 <kleberson55@hotmail.com>
2025-06-04 15:01:15 -0300

    editando o return do toString da classe Person

--- app/src/main/java/com/kleberson/listadecursos/model/Person.kt
+++ app/src/main/java/com/kleberson/listadecursos/model/Person.kt
@@ -2,6 +2,6 @@ package com.kleberson.listadecursos.model
 
 class Person (val firstName: String,val lastName: String, val course: String, val contact: String) {
     override fun toString(): String {
-        return "Usuario Salvo: $firstName $lastName, Curso: $course, Contato: $contact"
+        return " $firstName $lastName, Curso: $course, Contato: $contact"
     }
 }
