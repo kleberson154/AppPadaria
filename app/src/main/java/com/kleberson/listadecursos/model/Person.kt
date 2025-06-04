60fbb4feb986882f95d53c9d5c9ab13efa3aa1d9
kleberson154 <kleberson55@hotmail.com>
2025-06-04 00:09:12 -0300

    Adicionando os gets da classe Person

--- app/src/main/java/com/kleberson/listadecursos/model/Person.kt
+++ app/src/main/java/com/kleberson/listadecursos/model/Person.kt
@@ -1,6 +1,11 @@
 package com.kleberson.listadecursos.model
 
-class Person (val firstName: String, val lastName: String, val course: String, val contact: String) {
+class Person (private val firstName: String,private val lastName: String, private val course: String, private val contact: String) {
+    fun getFirstName() = firstName
+    fun getLastName() = lastName
+    fun getCourse() = course
+    fun getContact() = contact
+
     override fun toString(): String {
         return "Usuario Salvo: $firstName $lastName, Curso: $course, Contato: $contact"
     }
