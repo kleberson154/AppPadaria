445077582e4fb8b17923c57ddd8a0c7ad8c76a1e
kleberson154 <kleberson55@hotmail.com>
2025-06-04 15:02:06 -0300

    Coletando os elementos da activity e adicionando os eventos e funcoes aos devidos botoes

--- app/src/main/java/com/kleberson/listadecursos/view/MainActivity.kt
+++ app/src/main/java/com/kleberson/listadecursos/view/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.kleberson.listadecursos.view
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kleberson.listadecursos.R
-import com.kleberson.listadecursos.model.Person
+import com.kleberson.listadecursos.controller.PersonController
 
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,60 +25,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun salvar(view: View){
         val firstNameInput = findViewById<EditText>(R.id.firstNameInput)
         val lastNameInput = findViewById<EditText>(R.id.lastNameInput)
         val nameCourseInput = findViewById<EditText>(R.id.nameCourseInput)
         val contactInput = findViewById<EditText>(R.id.contactInput)
-        val person = Person(firstNameInput.text.toString(),
-            lastNameInput.text.toString(),
-            nameCourseInput.text.toString(),
-            contactInput.text.toString())
-        savePreferences(person)
+        val buttonSalvar = findViewById<Button>(R.id.button_Salvar)
+        val buttonLimpar = findViewById<Button>(R.id.button_Limpar)
+        val buttonFinalizar = findViewById<Button>(R.id.button_Finalizar)
 
-        firstNameInput.setText("")
-        lastNameInput.setText("")
-        nameCourseInput.setText("")
-        contactInput.setText("")
+        buttonSalvar.setOnClickListener(View.OnClickListener { view ->
+            val personController = PersonController(this)
+            personController.toString()
+            personController.salvar(view, firstNameInput, lastNameInput, nameCourseInput, contactInput)
+        })
 
-        Toast.makeText(this, "Dados Salvos", Toast.LENGTH_SHORT).show()
+        buttonLimpar.setOnClickListener(View.OnClickListener { view ->
+            val personController = PersonController(this)
+            personController.toString()
+            personController.limpar(view, firstNameInput, lastNameInput, nameCourseInput, contactInput)
+        })
+
+        buttonFinalizar.setOnClickListener(View.OnClickListener { view ->
+            val personController = PersonController(this)
+            personController.toString()
+            personController.finalizar(view)
+        })
+
     }
-
-    fun limpar(view: View){
-        val firstNameInput = findViewById<EditText>(R.id.firstNameInput)
-        val lastNameInput = findViewById<EditText>(R.id.lastNameInput)
-        val nameCourseInput = findViewById<EditText>(R.id.nameCourseInput)
-        val contactInput = findViewById<EditText>(R.id.contactInput)
-
-        firstNameInput.setText("")
-        lastNameInput.setText("")
-        nameCourseInput.setText("")
-        contactInput.setText("")
-        clearPreferences()
-    }
-
-    fun finalizar(view: View) {
-        Toast.makeText(this, "Volte Sempre", Toast.LENGTH_SHORT).show()
-        finish()
-    }
-
-    fun savePreferences(person: Person) {
-        val sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("firstName", person.firstName.toString())
-        editor.putString("lastName", person.lastName.toString())
-        editor.putString("nameCourse", person.course.toString())
-        editor.putString("contact", person.contact.toString())
-        editor.apply()
-    }
-
-    fun clearPreferences() {
-        val sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.clear()
-        editor.apply()
-    }
-
 }
